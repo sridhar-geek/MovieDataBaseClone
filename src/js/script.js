2730ef9e4877ffac692914ef7a8ec1d816eb485b
Sridhar <sridharmoturu@gmail.com>
2023-05-31 20:20:38 +0530

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const global={
+    currentPage: window.location.pathname,
+    search:{
+        term: "",
+        type:"",
+        page:1,
+        totalPages:1,
+        totalResults:0,
+    },
+    api:{
+
+    }
+}
